c61824e6d326baebd480fecc0bef5212ed6b659c
Dum <dum999@gmail.com>
2017-04-02 16:22:26 +0700

    first swift project

---
+++ Swift/MyProject/MyProjectTests/MyProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  MyProjectTests.swift
+//  MyProjectTests
+//
+//  Created by Jirapong Charoentim on 4/2/2560 BE.
+//  Copyright © 2560 Jirapong Charoentim. All rights reserved.
+//
+
+import XCTest
+@testable import MyProject
+
+class MyProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
