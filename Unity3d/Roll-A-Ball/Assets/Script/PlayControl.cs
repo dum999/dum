6af0b98a01f196e0167d9f8f69917fee4b1989b6
Dum <dum999@gmail.com>
2017-07-04 14:05:12 +0700

    JaiD Roll-A-Ball

---
+++ Unity3d/Roll-A-Ball/Assets/Script/PlayControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayControl : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	private int count;
+
+	public Text countText;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float z = Input.GetAxis ("Vertical") * Time.deltaTime * speed;
+		float x = Input.GetAxis ("Horizontal") * Time.deltaTime * speed;
+
+		Vector3 movement = new Vector3 (x, 0, z);
+
+		rb.AddForce (movement);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Box")) {
+			other.gameObject.SetActive (false);
+			count++;
+			countText.text = "Score: " + count;
+		}
+	}
+}
