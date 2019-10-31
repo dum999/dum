1dbdc0efe739341c072d80f5855e1616af75041c
Jirapong <dum999@gmail.com>
2019-10-31 16:41:22 +0700

    More Java Source Dir

---
+++ android/MoreJavaDir/app/src/main/java/com/dum/more_java_dir/MainActivity.java
@@ -0,0 +1,18 @@
+package com.dum.more_java_dir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView text = findViewById(R.id.txt_text);
+        text.setText(Test.TEST);
+    }
+}
---
+++ android/MoreJavaDir/app/src_more/release/java/com/dum/more_java_dir/Test.java
@@ -0,0 +1,5 @@
+package com.dum.more_java_dir;
+
+public class Test {
+    public static final String TEST = "RELEASE";
+}
