7723e20c0321b631499e277f64119dccb24a0d41
Jirapong <dum999@gmail.com>
2019-10-31 17:12:31 +0700

    Settings

---
+++ android/Settings/app/src/main/java/com/dum/settings/MainActivity.java
@@ -0,0 +1,26 @@
+package com.dum.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings_container, new MySettingsFragment())
+                .commit();
+    }
+
+    public static class MySettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.preferences, rootKey);
+        }
+    }
+}
